1ae0c2bfe88dd11ec23bf097adbb5ddde8ede35b
Hanley Weng <hanleyweng@gmail.com>
2018-09-22 19:19:21 -0400

    ~ removed some unused code

--- AR TextPad/ViewController.swift
+++ AR TextPad/ViewController.swift
@@ -71,15 +71,6 @@ class ViewController: UIViewController, ARSCNViewDelegate, UITextViewDelegate {
 
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
     func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
         DispatchQueue.main.async {
             // UPDATE AR SCENE
@@ -110,12 +101,13 @@ class ViewController: UIViewController, ARSCNViewDelegate, UITextViewDelegate {
     
     func sessionWasInterrupted(_ session: ARSession) {
         // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        print("NOTICE: sessionWasInterrupted")
         
     }
     
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+        print("NOTICE: sessionInterruptionEnded")
     }
     
     ////////////////////////////////////////////////////////////////////////////////////////
